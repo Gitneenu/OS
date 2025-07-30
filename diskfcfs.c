8cee70ad2d971b42e79b34b179861fc300aea010
Gitneenu <147414487+Gitneenu@users.noreply.github.com>
2025-07-30 23:42:13 +0530

    Update diskfcfs.c

--- diskfcfs.c
+++ diskfcfs.c
@@ -1,36 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void fcfs(int r,int st,int request[r])
+void fcfs(int r, int st, int request[r])
 {
-	int seektime=0;
-	int parseek=0;
-	int current=st;
-	for(int i=0;i<r;i++)
-	{
-		parseek=abs(current-request[i]);
-		seektime=seektime+parseek;
-		current=request[i];
-	}
-	printf("Total seek time is %d: ",seektime);
+    int seektime = 0;
+    int parseek = 0;
+    int current = st;
+
+    printf("Seek sequence: %d", current); 
+
+    for (int i = 0; i < r; i++)
+    {
+        parseek = abs(current - request[i]);
+        seektime += parseek;
+        current = request[i];
+        printf(" -> %d", current); 
+    }
+
+    printf("\nTotal seek time is: %d\n", seektime);
 }
 
 int main()
 {
-	int r,st;
-	printf("Enter number of requests: ");
-	scanf("%d",&r);
-	
-	int request[r];
-	
-	printf("Enter request: ");
-	for (int i=0;i<r;i++)
-	{
-		scanf("%d",&request[i]);
-		
-	}
-	printf("Enter starting time: ");
-	scanf("%d",&st);
-	
-	fcfs(r,st,request);
+    int r, st;
+
+    printf("Enter number of requests: ");
+    scanf("%d", &r);
+
+    int request[r];
+
+    printf("Enter requests: ");
+    for (int i = 0; i < r; i++)
+    {
+        scanf("%d", &request[i]);
+    }
+
+    printf("Enter starting head position: ");
+    scanf("%d", &st);
+
+    fcfs(r, st, request);
+
+    return 0;
 }
